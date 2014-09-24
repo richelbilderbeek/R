243838bf590b2db24aa2a9e68e1f1b4c31c3a1c4
richelbilderbeek <richel@richelbilderbeek.nl>
2014-09-24 17:48:00 +0200

    Added RemoveColumnPredicate

--- RemoveColumnPredicate.r
+++ RemoveColumnPredicate.r
@@ -1,3 +1,14 @@
+# 1 2 3    1 3
+# 1 2 3    1 3
+# 1 2 3    1 3
+# 1 2 3    1 3
+# 1 2 3 -> 1 3
+# 1 2 3    1 3
+# 1 2 3    1 3
+# 1 2 3    1 3
+# 1 2 3    1 3
+# 1 2 3    1 3
+
 d <- data.frame(
     x = rep(1,10), 
     y = rep(2,10), 
@@ -5,7 +16,9 @@ d <- data.frame(
 )
 d
 
-sum <- sum(d$y) #20
-sum
+sum_to_delete <- sum(d$y) #20
+sum_to_delete
 
-e <- [, sum != sum_to_delete]
+e <- d[, colSums(d) != 20]
+e
+
