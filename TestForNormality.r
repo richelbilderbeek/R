2cada3c00be31cd2cc54b9121b80d3b686e0e289
richelbilderbeek <richel@richelbilderbeek.nl>
2014-09-25 12:10:42 +0200

    Normal distribution

--- TestForNormality.r
+++ TestForNormality.r
@@ -2,38 +2,40 @@
 rm(list = ls())
 
 # Characterics of the normal distrubution
-my_mean = 100.0
-my_sd = 10.0
+my_mean_expected = 100.0
+my_sd_expected = 10.0
 
 # Parameters for creating a normal distribution
-my_lower = my_mean - (3.0 * my_sd)
-my_upper = my_mean + (3.0 * my_sd)
-my_resolution = 10.0
-my_step = (my_upper - my_lower) / my_resolution
+# my_lower = my_mean_expected - (3.0 * my_sd_expected)
+# my_upper = my_mean_expected + (3.0 * my_sd_expected)
+# my_resolution = 10.0
+# my_step = (my_upper - my_lower) / my_resolution
 
 # Generate the xs, ys
-my_xs = x=seq(my_lower,my_upper,my_step)
-my_ys_expected = dnorm(my_xs,my_mean,my_sd) # The expected normal distribution
+# my_xs = x=seq(my_lower,my_upper,my_step)
+my_ys_expected = rnorm(10000,my_mean_expected,my_sd_expected)
 my_ys_measured = my_ys_expected + runif(length(my_ys_expected))
 
-my_table <- cbind(my_xs,my_ys_measured,my_ys_expected)
+my_table <- data.frame(cbind(my_ys_measured,my_ys_expected))
 my_table
 
-plot(my_ys_measured ~ my_xs,data = my_table)
-plot(c(my_ys_measured,my_ys_expected) ~ my_xs,data = my_table)
+my_mean_measured <- mean(my_table$my_ys_measured)
+my_sd_measured <- sd(my_table$my_ys_measured)
 
-plot(my_ys_expected ~ my_xs,data = my_table)
-plot(my_ys_measured ~ my_xs,data = my_table)
-
-my_distribution
-#shapiro.test(my_distibution)
-chisq.test(
-	my_distribution,
-	y = NULL, 
-	correct = TRUE,
-  p = my_table, 
-	rescale.p = FALSE,
-  simulate.p.value = FALSE, 
-	B = 2000
+hist(
+	my_table$my_ys_measured,
+	breaks = seq(50,150,1),
+	freq=FALSE,
+	main="Histogram of measured values",
+	xlab="Measured value",
+	ylab="Relative frequency"
 )
-?chisq.test
+curve(
+	dnorm(x,mean=my_mean_measured,sd=my_sd_measured),
+	col=rgb(1.0,0.75,0.0), #Orange
+	lwd=3,
+	add=T
+)
+
+bartlett.test(redox_calib ~ habitat, data = data6)
+
