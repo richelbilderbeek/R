0272a8933dd2a3b64e032db71ad462441b17d55e
richelbilderbeek <richel@richelbilderbeek.nl>
2015-02-18 13:57:03 +0100

    Created folder structure

---
+++ Functions/Browser.R
@@ -0,0 +1,17 @@
+IsPrime <- function(n)
+{
+  if (n < 0) { browser() }
+  
+  #Stub of IsPrime function
+  return (1)
+}
+
+Main <- function()
+{
+  IsPrime(3)
+  IsPrime(5)
+  IsPrime(-1)
+  IsPrime(7)
+}
+
+Main()
