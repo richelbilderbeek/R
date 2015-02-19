8ced54bc889b8d73f0ef45b9affff137eb6d8428
richelbilderbeek <richel@richelbilderbeek.nl>
2015-02-19 14:15:36 +0100

    Added CreateRandomDnaSequences

--- Create/CreateRandomDnaSequence.R
+++ Create/CreateRandomDnaSequence.R
@@ -1,5 +1,6 @@
 library(testit)
 
+# From https://github.com/richelbilderbeek/R
 CreateRandomBase <- function()
 {
   i <- sample(1:4,1)
@@ -11,6 +12,7 @@ CreateRandomBase <- function()
   return (NULL)
 }
 
+# From https://github.com/richelbilderbeek/R
 CreateRandomDnaSequence <- function(n_bases)
 {
   if (n_bases < 1)
@@ -26,19 +28,52 @@ CreateRandomDnaSequence <- function(n_bases)
   return (sequence)
 }
 
+# From https://github.com/richelbilderbeek/R
+CreateRandomDnaSequences <- function(n_sequences,n_bases)
+{
+  if (n_sequences < 1)
+  {
+    print("ERROR: CreateRandomDnaSequences: n_sequences cannot be less than 1")
+    return (NULL)
+  }
+  if (n_bases < 1)
+  {
+    print("ERROR: CreateRandomDnaSequences: n_bases cannot be less than 1")
+    return (NULL)
+  }
+  sequences <- NULL
+  for (i in 1:n_sequences)
+  {
+    sequences <- rbind(sequences,CreateRandomDnaSequence(n_bases)) 
+  }
+  return (sequences)
+}
+
+# From https://github.com/richelbilderbeek/R
 Test <- function()
 {
+  # CreateRandomBase
   {
     b <- CreateRandomBase()
     assert(nchar(b) == 1)
   }
-
+  # CreateRandomDnaSequence
   {
     n_bases <- 10
     s <- CreateRandomDnaSequence(n_bases)
     assert(nchar(s) == n_bases)
   }
+  # CreateRandomDnaSequences
+  {
+    n_sequences <- 2
+    n_bases <- 5
+    s <- CreateRandomDnaSequences(n_sequences,n_bases)
+    assert(length(s) == n_sequences)
+    assert(nchar(s[1]) == n_bases)
+  }
   print("Testing done")
 }
 
 Test()
+
+print(CreateRandomDnaSequences(2,10))
