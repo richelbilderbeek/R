f112f13e9fdbb9488ee2fdab82bf1791d1665296
richelbilderbeek <richel@richelbilderbeek.nl>
2015-04-07 15:14:22 +0200

    Added library path

---
+++ Packages/AddLibraryPaths.R
@@ -0,0 +1,18 @@
+
+path_to_add <- "/home/richel/R/i686-pc-linux-gnu-library/3.1"
+
+print(.libPaths())
+
+lib_paths_before <- length(.libPaths())
+
+# Only adds folder if it exists
+.libPaths(c(.libPaths(),path_to_add))
+
+lib_paths_after <- length(.libPaths())
+
+if (lib_paths_before < lib_paths_after)
+{
+  print(paste("Successfully added ",path_to_add," to paths",sep=""))
+} else {
+  print(paste("Failed to add ",path_to_add," to paths",sep=""))
+}
