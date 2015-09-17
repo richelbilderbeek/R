b806316fbecba50b715b5daae859d8c7318d5aee
richelbilderbeek <richel@richelbilderbeek.nl>
2015-09-17 09:22:15 +0200

    Fixed script

--- DownloadApe.sh
+++ DownloadApe.sh
@@ -5,22 +5,23 @@ then
   mkdir MyFavoritePackages
 fi
 
-APE_FILE=ape_3.3.tar.gz
+FILE=ape_3.3.tar.gz
+FOLDER=ape
 
-if [ ! -e MyFavoritePackages/$APE_FILE ]
+if [ ! -e MyFavoritePackages/$FILE ]
 then
-  echo "Downloading plyr tar"
-  wget https://cran.r-project.org/src/contrib/$APE_FILE
-  mv $APE_FILE MyFavoritePackages/$APE_FILE
+  echo "Downloading tar"
+  wget https://cran.r-project.org/src/contrib/$FILE
+  mv $FILE MyFavoritePackages/$FILE
 else
   echo "tar already on disk"
 fi
 
-if [ ! -d MyFavoritePackages/plyr ]
+if [ ! -d MyFavoritePackages/$FOLDER ]
 then
-  echo "Unpacking plyr tar"
+  echo "Unpacking tar"
   cd MyFavoritePackages
-  tar zxvf $APE_FILE
+  tar zxvf $FILE
   cd ..
 else
   echo "tar already unpacked"
