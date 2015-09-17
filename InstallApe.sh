d665482c34e51f4ac901c9fa5803b764e7bba877
richelbilderbeek <richel@richelbilderbeek.nl>
2015-09-17 10:17:50 +0200

    Switch on computer type

--- InstallApe.sh
+++ InstallApe.sh
@@ -2,4 +2,28 @@
 
 ./DownloadApe.sh
 
-R CMD INSTALL ./MyFavoritePackages/ape
+if [ HOSTNAME ]
+then
+  echo "HOSTNAME: "$HOSTNAME
+else
+  echo "HOSTNAME not found"
+fi
+
+if [ "fwn-biol-132-102" == "$HOSTNAME" ]
+then
+  echo "My working computer"
+  R CMD INSTALL ./MyFavoritePackages/ape
+else
+  echo "This is not my working computer"
+fi
+
+if [ "pg-login" == "$HOSTNAME" ]
+then
+  echo "Peregine login computer"
+  module load R/3.1.2-goolfc-2.7.11-default
+  R CMD INSTALL ./MyFavoritePackages/ape
+else
+  echo "This is not my working computer"
+fi
+
+
