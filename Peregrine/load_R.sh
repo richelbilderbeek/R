8e40e4ff6b86de1fbae4b68eaf292c9da9ec9b3f
richelbilderbeek <richel@richelbilderbeek.nl>
2015-09-17 11:08:20 +0200

    The script shares its wisdom

--- Peregrine/load_R.sh
+++ Peregrine/load_R.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+echo "This is a useless script, as the module is unloaded at the termination of it..."
+
 module load R/3.1.2-goolfc-2.7.11-default
+
+echo "Just type: "
+
+echo "'module load R/3.' and then use tab"
