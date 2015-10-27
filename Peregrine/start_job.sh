38198e88d5fb20431a4f609007c0be1cb576cd0a
richelbilderbeek <richel@richelbilderbeek.nl>
2015-10-27 14:18:51 +0100

    Fixed

--- Peregrine/start_job.sh
+++ Peregrine/start_job.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-#SBATCH --time=0-00:01:00
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --ntasks=1
-#SBATCH --mem=1000
-#SBATCH --job-name=test_job_1
-#SBATCH --mail-type=BEGIN,END
-#SBATCH --partition=short
-module load R/3.2.1-goolfc-2.7.11-default-X11
-Rscript run.R
+sbatch ./job.sh
