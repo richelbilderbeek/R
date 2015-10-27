0f4bb3b966c51cedcb4dd065e8c1e799498cd752
richelbilderbeek <richel@richelbilderbeek.nl>
2015-10-27 14:31:58 +0100

    Going up a notch

--- Peregrine/job.sh
+++ Peregrine/job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --time=0-00:01:00
+#SBATCH --time=0-00:10:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --ntasks=1
-#SBATCH --mem=1000
-#SBATCH --job-name=test_job_1
+#SBATCH --mem=10000
+#SBATCH --job-name=test_job_2
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --partition=short
 module load R/3.2.1-goolfc-2.7.11-default-X11
