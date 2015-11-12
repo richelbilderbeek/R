5984e1aabcd6e8d0f6ace85fef7e241468358126
richelbilderbeek <richel@richelbilderbeek.nl>
2015-11-12 14:35:32 +0100

    Use BEAGLE

--- Peregrine/job.sh
+++ Peregrine/job.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#SBATCH --time=0-00:10:00
+#SBATCH --time=16:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --ntasks=1
 #SBATCH --mem=10000
 #SBATCH --job-name=test_job_2
 #SBATCH --mail-type=BEGIN,END
-#SBATCH --partition=short
-module load R Beast
+module load R beagle-lib Beast
 Rscript run.R
