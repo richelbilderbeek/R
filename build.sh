a58d781460807587f2fc75ece2b66eaf5d0b9dc0
richelbilderbeek <richel@richelbilderbeek.nl>
2015-12-03 13:34:09 +0100

    Added build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./DownloadAllMyFavoritePackages.sh
+./DownloadApe.sh
+./DownloadDeSolve.sh
+./DownloadGeiger.sh
+./DownloadOlliRbeast.sh
+./DownloadPbd.sh
+./DownloadPhangorn.sh
+./DownloadPhyloch.sh
+./DownloadPlyr.sh
+./InstallApe.sh
