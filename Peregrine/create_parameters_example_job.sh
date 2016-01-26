7a950fca5f6c46b6b8c8982e1aa6c04958a5ce92
richelbilderbeek <richel@richelbilderbeek.nl>
2016-01-26 10:59:01 +0100

    Renamed

--- Peregrine/create_parameters_example_job.sh
+++ Peregrine/create_parameters_example_job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --time=0:01:00
+#SBATCH --time=0:10:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --ntasks=1
 #SBATCH --mem=100000
-#SBATCH --job-name=create_parameters_article
+#SBATCH --job-name=create_parameters_example
 #SBATCH --mail-type=BEGIN,END
 module load R
-./create_parameters_article.sh
+./create_parameters_example.sh
--- Peregrine/create_parameters_toy_example_job.sh
+++ Peregrine/create_parameters_toy_example_job.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --ntasks=1
 #SBATCH --mem=100000
-#SBATCH --job-name=create_parameters_article
+#SBATCH --job-name=create_parameters_toy_example
 #SBATCH --mail-type=BEGIN,END
 module load R
-./create_parameters_article.sh
+./create_parameters_toy_example.sh
