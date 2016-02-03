4d8f1d36814359072d89453cb1bb1ff47a432eca
richelbilderbeek <richel@richelbilderbeek.nl>
2016-02-03 16:14:19 +0100

    EOD

---
+++ Syntax/LogicalAndVersusBitwiseAnd.R
@@ -0,0 +1,24 @@
+library(testit)
+z = 1:6
+assert(all.equal( (z > 2) & (z < 5) , c(FALSE,FALSE,TRUE,TRUE,FALSE,FALSE) ) )
+assert(all.equal( z[(z>2) & (z<5)]  , c(3,4) ) )
+assert(all.equal( (z > 2) && (z < 5) , c(FALSE) ) )
+assert(all.equal(  z > 2  &&  z < 5  , c(FALSE) ) )
+assert(all.equal( z[(z > 2) && (z < 5)] , c(0) ) )
+assert(all.equal( z[ z > 2  &&  z < 5 ] , c(0) ) )
+
+say_hello <- function() 
+{
+  print("Hello")
+  return (1)
+}
+
+# say_hello should not be called
+# operator& evaluates all arguments, even if the first one is false
+# operator&& evaluates the arguments until it founds a false (and thus can never be true anymore)
+if (1 + 1 == 3  & say_hello()) { }
+if (1 + 1 == 3 && say_hello()) { }
+
+# Also for more arguments
+if (1 + 1 == 3  & 1 + 1 == 4  & say_hello()) { }
+if (1 + 1 == 3 && 1 + 1 == 4 && say_hello()) { }
