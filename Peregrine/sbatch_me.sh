762f2bc871e64fadccf05d47b25643f250364667
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-29 17:32:37 +0200

    Use 4 cores per node

--- Peregrine/sbatch_me.sh
+++ Peregrine/sbatch_me.sh
@@ -2,8 +2,8 @@
 # Call with e.g. 'sbatch ./sbatch_me do_simulation.R example1.RDa'
 #SBATCH --time=240:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks=4
 #SBATCH --mem=10G
 #SBATCH --job-name=article
 #SBATCH --mail-type=BEGIN,END
